528f65943fb171481c26ab510744642900802bf6
ndxbxrme <lewis_the_cat@hotmail.com>
2024-09-04 17:22:39 +0100

    disable messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'components/notification_class.dart';
 import 'components/notification_controller.dart';
